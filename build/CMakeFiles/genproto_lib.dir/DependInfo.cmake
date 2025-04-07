5070d1c9acaaea46bddbbd839a78cc260af8be2e
asad <asadbahtiyarov2002@gmail.com>
2025-04-08 00:50:09 +0500

    grpc server 0.0.1 version

---
+++ build/CMakeFiles/genproto_lib.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/asad/cpp/protobuf/genproto/order_service/order.grpc.pb.cc" "CMakeFiles/genproto_lib.dir/genproto/order_service/order.grpc.pb.cc.o" "gcc" "CMakeFiles/genproto_lib.dir/genproto/order_service/order.grpc.pb.cc.o.d"
+  "/home/asad/cpp/protobuf/genproto/order_service/order.pb.cc" "CMakeFiles/genproto_lib.dir/genproto/order_service/order.pb.cc.o" "gcc" "CMakeFiles/genproto_lib.dir/genproto/order_service/order.pb.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
